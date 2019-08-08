8e2741f99746bc34fab6f4bf0d79c7d9367079d7
Northisland <shihomi0104@icloud.com>
2019-08-08 14:12:29 -0700

    after francois tell me how to fix it

--- DAO.java
+++ DAO.java
@@ -11,7 +11,7 @@ public class DAO {
 	public static void main(String[] args) {
 		DAO obj_Dao = new DAO();
 		obj_Dao.check_Data();
-		obj_Dao.update();
+		obj_Dao.save();
 	}
 	
 	public void check_Data() {
@@ -37,90 +37,64 @@ public class DAO {
 		}
 	}
 	
-public int update() {
+public int save() {
 		
-		int status=0;
-		int idCountry =0;
+		int status= 0;
+		int idCountry = 0;
 		String country_Name = "";
+		
+		DB_Connection objDb_Connection = new DB_Connection();
+		Connection connection = objDb_Connection.getConnection();
+		PreparedStatement ps = null;
 		
 		try{
 			Connection con= DB_Connection.getConnection();
 			
-			//PreparedStatement ps = con.prepareStatement("select CountryName from Country");
-			PreparedStatement ps = con.prepareStatement("select * from Country");
-			 
-			ResultSet rs = ps.executeQuery();
+			String query = "INSERT INTO `Country`(`CountryName`) VALUES (?)";
+			ps = connection.prepareStatement(query);
+			ps.setString(1, country_Name);
+			//System.out.println(query);
 			
-			if(rs.next()){
-				idCountry=rs.getInt("id");
-				country_Name = rs.getString("countryName");
-			}
-			
-			if( idCountry > 0){
-			PreparedStatement ps2 = con.prepareStatement("update country set CountryName = ?");
-			ps2.setString(1, country_Name);
-			
-			status = ps2.executeUpdate();
-			}
+			// this is problem.
+			int rs = ps.executeUpdate();
 			con.close();
+		
 		}catch(Exception e){System.out.println(e);}
 		
 		return status;
 	}
 
 	
-//	public int update() {
-//		
-//		int status=0;
-//		int idCountry = 0;
-//		String countryName = "";
-//		
-//		try{
-//			Connection con= DB_Connection.getConnection();
-//			
-//			//PreparedStatement ps = con.prepareStatement("select CountryName from Country");
-//			PreparedStatement ps = con.prepareStatement("select CountryName from Country");
-//			 
-//			ResultSet rs = ps.executeQuery();
-//			
-//			if(rs.next()){
-//				idCountry=rs.getInt("id");
-//				countryName = rs.getString("countryName");
-//			}
-//			
-//			if( idCountry > 0){
-//			PreparedStatement ps2 = con.prepareStatement("update CountryName, CountryName = ?");
-//			ps2.setString(1, countryName);
-//			
-//			status = ps2.executeUpdate();
-//			}
-//			con.close();
-//		}catch(Exception e){System.out.println(e);}
-//		
-//		return status;
-//	}
+public static int update(){
 	
-//	public static int save(String bookcallno,int studentid,String studentname,String studentcontact){
-//		int status=0;
-//		try{
-//			Connection con=DB_Connection.getConnection();
-//			
-//			status= update(bookcallno);//updating quantity and issue
-//			
-//			if(status>0){
-//			PreparedStatement ps=con.prepareStatement("insert into issuebooks(bookcallno,studentid,studentname,studentcontact) values(?,?,?,?)");
-//			ps.setString(1,bookcallno);
-//			ps.setInt(2,studentid);
-//			ps.setString(3,studentname);
-//			ps.setString(4,studentcontact);
-//			status=ps.executeUpdate();
-//			}
-//			
-//			con.close();
-//			
-//		}catch(Exception e){System.out.println(e);}
-//		return status;
-//	}
+	int status=0;
+	int quantity=0,issued=0;
+	DB_Connection objDb_Connection = new DB_Connection();
+	
+	try{
+		Connection con= objDb_Connection.getConnection();
+		
+		PreparedStatement ps=con.prepareStatement("select quantity,issued from books where callno=?");
+		ps.setString(1,);
+		ResultSet rs=ps.executeQuery();
+		if(rs.next()){
+			quantity=rs.getInt("quantity");
+			issued=rs.getInt("issued");
+		}
+		
+		if(quantity>0){
+		PreparedStatement ps2=con.prepareStatement("update books set quantity=?,issued=? where callno=?");
+		ps2.setInt(1,quantity-1);
+		ps2.setInt(2,issued+1);
+		ps2.setString(3,bookcallno);
+		
+		status=ps2.executeUpdate();
+		}
+		con.close();
+	}catch(Exception e){System.out.println(e);}
+	return status;
+}
+	
 //	public static int Delete(String bookcallno,int studentid){
 //		int status=0;
 //		try{
